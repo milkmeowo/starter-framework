a1fb6c8b92d62364765b0e82f2008c3a894e4333
milkmeowo <milkmeowo@gmail.com>
2016-12-30 16:05:17 +0800

    Implement Custom Messages & Attributes

---
+++ src/Framework/Repository/Foundations/Validators/Validator.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Milkmeowo\Framework\Repository\Foundations\Validators;
+
+use Prettus\Validator\LaravelValidator;
+
+class Validator extends LaravelValidator
+{
+
+    /**
+     * Validation Custom Messages
+     *
+     * @var array
+     */
+    protected $messages = [];
+
+    /**
+     * Validation Custom Attributes
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * Get Custom error messages for validation
+     *
+     * @return array
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Set Custom error messages for Validation
+     * @param array $messages
+     *
+     * @return $this
+     */
+    public function setMessages(array $messages)
+    {
+        $this->messages = $messages;
+        return $this;
+    }
+
+    /**
+     * Get Custom error attributes for validation
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Set Custom error attributes for Validation
+     *
+     * @param array $attributes
+     *
+     * @return $this
+     */
+    public function setAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+        return $this;
+    }
+
+    /**
+     * Pass the data and the rules to the validator
+     *
+     * @param string $action
+     * @return bool
+     */
+    public function passes($action = null)
+    {
+        $rules      = $this->getRules($action);
+        $messages   = $this->getMessages();
+        $attributes = $this->getAttributes();
+        $validator  = $this->validator->make($this->data, $rules, $messages, $attributes);
+        if( $validator->fails() )
+        {
+            $this->errors = $validator->messages();
+            return false;
+        }
+        return true;
+    }
+
+}
