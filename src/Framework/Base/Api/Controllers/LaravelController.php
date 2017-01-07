b3af34e7a63584de0d9b40feeec5241e512be8e6
milkmeowo <milkmeowo@gmail.com>
2017-01-07 23:32:52 +0800

    fix(laravel): fix ValidatesRequests namespace

--- src/Framework/Base/Api/Controllers/LaravelController.php
+++ src/Framework/Base/Api/Controllers/LaravelController.php
@@ -6,10 +6,11 @@ use Dingo\Api\Routing\Helpers as DingoHelper;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
+use Milkmeowo\Framework\Base\Validation\ValidatesRequests;
 
 abstract class LaravelController extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, Milkmeowo\Base\Validation\ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     /* Dingo API Helper */
     use DingoHelper;
 }
