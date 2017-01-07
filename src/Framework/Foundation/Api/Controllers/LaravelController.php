8d1a78ad504273c3a305c1b861b55f4489416639
milkmeowo <milkmeowo@gmail.com>
2017-01-07 12:09:20 +0800

    refactor(laravel): refactor LaravelController
    * Rewrite ValidatesRequests
    * Add AuthorizesRequests, DispatchesJobs, ValidatesRequests trait

--- src/Framework/Foundation/Api/Controllers/LaravelController.php
+++ src/Framework/Foundation/Api/Controllers/LaravelController.php
@@ -3,10 +3,14 @@
 namespace Milkmeowo\Framework\Foundation\Api\Controllers;
 
 use Dingo\Api\Routing\Helpers as DingoHelper;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
+use Milkmeowo\Foundation\Validation\ValidatesRequests;
 
 abstract class LaravelController extends BaseController
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     /* Dingo API Helper */
     use DingoHelper;
 }
