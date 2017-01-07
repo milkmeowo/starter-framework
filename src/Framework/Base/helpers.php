c166efbdeec455ff4d07c73beafd3e0cc8cd42d2
milkmeowo <milkmeowo@gmail.com>
2017-01-07 23:42:42 +0800

    feat(helpers): add smart_get_client_ip

--- src/Framework/Base/helpers.php
+++ src/Framework/Base/helpers.php
@@ -19,3 +19,20 @@ if (! function_exists('app_path')) {
         return app('path').($path ? DIRECTORY_SEPARATOR.$path : $path);
     }
 }
+
+if (! function_exists('smart_get_client_ip')) {
+
+    /**
+     * @return array|string
+     */
+    function smart_get_client_ip()
+    {
+        $request = app('request');
+        $clientIp = $request->header('X-Client-Ip');
+        if (empty($clientIp)) {
+            $clientIp = $request->getClientIp(true);
+        }
+
+        return $clientIp;
+    }
+}
