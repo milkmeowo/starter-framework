a28f2ad1e64c199daa4dc14d2ae6ae8800c7eb7a
milkmeowo <milkmeowo@gmail.com>
2017-01-10 18:30:53 +0800

    refactor: change  pagination handle

--- src/Framework/Dingo/Listeners/AddPaginationLinksToResponse.php
+++ src/Framework/Dingo/Listeners/AddPaginationLinksToResponse.php
@@ -17,9 +17,18 @@ class AddPaginationLinksToResponse
         if (isset($event->content['meta']['pagination'])) {
             $links = $event->content['meta']['pagination']['links'];
 
+            $header = '';
+            if (isset($links['next'])) {
+                $header .= '<'.$links['next'].'>; rel="next"';
+            }
+
+            if (isset($links['previous'])) {
+                $header .= ', <'.$links['previous'].'>; rel="prev"';
+            }
+
             $event->response->headers->set(
                 'link',
-                sprintf('<%s>; rel="next", <%s>; rel="prev"', $links['links']['next'], $links['links']['previous'])
+                $header
             );
         }
     }
